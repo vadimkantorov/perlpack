8de6309a8f58a8f82d28cc5c551b4168709d1966
Vadim Kantorov <vadimkantorov@gmail.com>
2024-08-17 01:26:06 +0200

    ...

--- packfs.h
+++ packfs.h
@@ -1,7 +1,3 @@
 int packfsfilesnum = 0, packfsdirsnum = 0;
-
-// extern char _binary_{filepathwithunderscoresaspathsep}_start[], _binary_{filepathwithunderscoresaspathsep}_end[]; 
-
 struct packfsinfo { const char* safe_path; const char *path; const char* start; const char* end; } packfsinfos[] = {};
-
 const char* packfsdirs[] = {};
