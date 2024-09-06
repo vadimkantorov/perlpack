5ecf2b335252c7adc7b186dbaf308184473b8994
Vadim Kantorov <vadimkantorov@gmail.com>
2024-09-06 13:19:23 +0200

    ...

--- perlpack.h
+++ perlpack.h
@@ -1,3 +1,5 @@
-int packfsfilesnum, packfsdirsnum;
-struct packfsinfo { const char* safe_path; const char *path; const char* start; const char* end; } packfsinfos[] = {};
-const char* packfsdirs[] = {};
+int packfsinfosnum;
+const char* packfs_safepaths[] = {};
+const char* packfs_abspaths[] = {};
+const char* packfs_starts[] = {};
+const char* packfs_ends[] = {};
