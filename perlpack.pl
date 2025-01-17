f8451ed9209c51478e0886d5dd6a4c7de9345870
Vadim Kantorov <vadimkantorov@gmail.com>
2025-01-17 18:49:47 +0200

    ...

--- perlpack.pl
+++ perlpack.pl
@@ -64,7 +64,7 @@ File::Find::find(sub {
         die "File should not end with .o" if substr($relpath, -2) eq '.o';
         
         chdir($output_path_o);
-        symlink($p, $safepath);
+        symlink(File::Spec->catfile($oldcwd, $p), $safepath);
         print($p," ", $safepath, " ", $relpath, "\n", `ls`);
         system($ld, '-r', '-b', 'binary', '-o', $objects[-1], $safepath) == 0 or die "ld command failed: $?";
         unlink($safepath);
