0bccd8fdb1d2611af1067e3b33fe5a8affd97b35
Vadim Kantorov <vadimkantorov@gmail.com>
2025-01-17 18:37:06 +0200

    ...

--- perlpack.pl
+++ perlpack.pl
@@ -65,6 +65,7 @@ File::Find::find(sub {
         
         symlink($relpath, File::Spec->catfile($output_path_o, $safepath));
         chdir($output_path_o);
+        print($relpath, "\n", `ls`);
         system($ld, '-r', '-b', 'binary', '-o', $objects[-1], $safepath) == 0 or die "ld command failed: $?";
         unlink($safepath);
     }
